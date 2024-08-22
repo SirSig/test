85ce3822a1c3e903894d705410d532179a412e23
SirSig <91037591+SirSig@users.noreply.github.com>
2024-08-22 12:01:59 +1000

    Create HttpErrorReport.js

---
+++ HttpErrorReport.js
@@ -0,0 +1,10 @@
+export class HttpErrorReport extends Error {
+    constructor(message, status, statusText, url) {
+        super(message);
+        this.name = this.constructor.name;
+        this.message = message;
+        this.status = status;
+        this.statusText = statusText;
+        this.url = url;
+    }
+}
